bcdb81c6ee0131572d3b65c6a2116ced06111f5a
BT1276 <99468952+BT1276@users.noreply.github.com>
2022-02-11 15:06:45 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,13 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
+return `PAYMENT BY BT1276
 
 *📲 PULSA*
 
-Tidak tersedia
+1. TELKOMSEL
+    082147334450
+ 
+ 2. XL
+     087742789852
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
